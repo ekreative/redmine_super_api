b40dfba27386fc0f8f7b7f9bb542c16447a18aec
Yura Paliy <ypalii2012@gmail.com>
2015-08-26 22:33:46 +0300

    added pager

--- app/controllers/tprojects_controller.rb
+++ app/controllers/tprojects_controller.rb
@@ -1,13 +1,16 @@
 class TprojectsController < ApplicationController
   unloadable
+  accept_api_auth :index
 
+  def index
 
-  def getprojects
+    @offset, @limit = api_offset_and_limit
 
-    if params['limit'] and params['offset']
-      @limit = "LIMIT " + params['limit']
-      @offset = "OFFSET " + params['offset']
+    if @offset and @limit
+      @limit = "LIMIT #{@limit}"
+      @offset = "OFFSET #{@offset}"
     end
+
 
     if params['ordering']
       @ordering = "ORDER BY p.name " + params['ordering']
@@ -22,17 +25,26 @@ class TprojectsController < ApplicationController
       LEFT JOIN (SELECT cv.value AS IOS,cv.customized_id FROM custom_fields as cf LEFT JOIN custom_values AS cv ON cv.custom_field_id = cf.id WHERE cf.name = 'IOS') AS ios ON ios.customized_id = p.id
       LEFT JOIN (SELECT cv.value AS Android,cv.customized_id FROM custom_fields as cf LEFT JOIN custom_values AS cv ON cv.custom_field_id = cf.id WHERE cf.name = 'Android') AS android ON android.customized_id = p.id
       #{@filter_name} GROUP BY p.id #{@ordering} #{@limit} #{@offset}
-                                               ")
+                                           ")
+    @total_count = @t_projects_list.count
 
-    render json: Tprojects.new(@t_projects_list,  params['offset'], params['limit'], 2)
+    respond_to do |format|
+      format.api  {
+        @offset, @limit = api_offset_and_limit
+        @project_count = @total_count
+        @projects = @t_projects_list
+      }
+    end
+
+    # render json: Tprojects.new(@t_projects_list, @offset, @limit, @total_count)
   end
 
 
   class Tprojects
 
     def initialize(projects, offset, limit, total_count)
-      @projects     = projects
-      @offset   = offset.to_i
+      @projects = projects
+      @offset = offset.to_i
       @limit = !limit ? 25 : limit.to_i
       @total_count = total_count
     end
@@ -40,6 +52,7 @@ class TprojectsController < ApplicationController
     def projects
       @projects
     end
+
     def offset
       @offset
     end
@@ -47,10 +60,10 @@ class TprojectsController < ApplicationController
     def limit
       @limit
     end
+
     def total_count
       @total_count
     end
-
 
   end
 
